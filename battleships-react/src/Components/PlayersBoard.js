28cf6493b2e5590a47ae953d01b3eb77923ef368
Fojilun Hussain <fojilun.hussain@investec.co.uk>
2019-10-31 12:10:36 +0000

    cells change state depending on if there is a boat in the computers board, display state on hit

--- battleships-react/src/Components/PlayersBoard.js
+++ battleships-react/src/Components/PlayersBoard.js
@@ -1,27 +1,46 @@
 import React from 'react';
 import Cells from './Cells'
 
+class Cell {
+    constructor(content) {
+        this.content = content
+        this.isHit = false
+    }
+    hit () {
+        this.isHit = true
+        if(this.content === "~") this.content = "/"
+        if(this.content === "SH") this.content = "X"
+    }
+    getContent() {
+        return this.isHit ? this.content : " "
+    }
+}
+
 function PlayersBoard() {
 
     const [playersBoard, setPlayersBoard] = React.useState([
-        ["","","",""],
+        [new Cell("~"),"","",""],
         ["","","",""],
         ["","","",""],
         ["","","",""]
     ])
 
     const [computersBoard, setComputersBoard] = React.useState([
-        ["","","",""],
-        ["","","",""],
-        ["","S","S",""],
-        ["","","",""]
+        [new Cell("~"),new Cell("~"),new Cell("~"),new Cell("SH")],
+        [new Cell("~"),new Cell("~"),new Cell("~"),new Cell("SH")],
+        [new Cell("~"),new Cell("~"),new Cell("~"),new Cell("SH")],
+        [new Cell("~"),new Cell("~"),new Cell("~"),new Cell("SH")]
     ])
 
     const onHit = (row, col) => {
-        const newPlayersBoard = playersBoard.slice()
-        newPlayersBoard[row][col] = "X"
-        setPlayersBoard(newPlayersBoard)
+        const newComputersBoard = computersBoard.slice()
+        newComputersBoard[row][col].hit()
+        setComputersBoard(newComputersBoard)
     }
+
+    // const checkHit = (row, col) => {
+    //     console.log(computersBoard[row][col])
+    // }
 
     return (
         <>
@@ -31,22 +50,22 @@ function PlayersBoard() {
                 gridTemplateRows: 'repeat(4, 50px)'
 
             }}>  
-            <Cells content = {playersBoard[0][0]} onHit = {() => onHit(0,0)}/>
-            <Cells content = {playersBoard[0][1]} onHit = {() => onHit(0,1)}/>
-            <Cells content = {playersBoard[0][2]} onHit = {() => onHit(0,2)}/>
-            <Cells content = {playersBoard[0][3]} onHit = {() => onHit(0,3)}/>
-            <Cells content = {playersBoard[1][0]} onHit = {() => onHit(1,0)}/>
-            <Cells content = {playersBoard[1][1]} onHit = {() => onHit(1,1)}/>
-            <Cells content = {playersBoard[1][2]} onHit = {() => onHit(1,2)}/>
-            <Cells content = {playersBoard[1][3]} onHit = {() => onHit(1,3)}/>
-            <Cells content = {playersBoard[2][0]} onHit = {() => onHit(2,0)}/>
-            <Cells content = {playersBoard[2][1]} onHit = {() => onHit(2,1)}/>
-            <Cells content = {playersBoard[2][2]} onHit = {() => onHit(2,2)}/>
-            <Cells content = {playersBoard[2][3]} onHit = {() => onHit(2,3)}/>
-            <Cells content = {playersBoard[3][0]} onHit = {() => onHit(3,0)}/>
-            <Cells content = {playersBoard[3][1]} onHit = {() => onHit(3,1)}/>
-            <Cells content = {playersBoard[3][2]} onHit = {() => onHit(3,2)}/>
-            <Cells content = {playersBoard[3][3]} onHit = {() => onHit(3,3)}/>
+            <Cells content = {computersBoard[0][0].getContent()} onHit = {() => onHit(0,0)}/>
+            <Cells content = {computersBoard[0][1].getContent()} onHit = {() => onHit(0,1)}/>
+            <Cells content = {computersBoard[0][2].getContent()} onHit = {() => onHit(0,2)}/>
+            <Cells content = {computersBoard[0][3].getContent()} onHit = {() => onHit(0,3)}/>
+            <Cells content = {computersBoard[1][0].getContent()} onHit = {() => onHit(1,0)}/>
+            <Cells content = {computersBoard[1][1].getContent()} onHit = {() => onHit(1,1)}/>
+            <Cells content = {computersBoard[1][2].getContent()} onHit = {() => onHit(1,2)}/>
+            <Cells content = {computersBoard[1][3].getContent()} onHit = {() => onHit(1,3)}/>
+            <Cells content = {computersBoard[2][0].getContent()} onHit = {() => onHit(2,0)}/>
+            <Cells content = {computersBoard[2][1].getContent()} onHit = {() => onHit(2,1)}/>
+            <Cells content = {computersBoard[2][2].getContent()} onHit = {() => onHit(2,2)}/>
+            <Cells content = {computersBoard[2][3].getContent()} onHit = {() => onHit(2,3)}/>
+            <Cells content = {computersBoard[3][0].getContent()} onHit = {() => onHit(3,0)}/>
+            <Cells content = {computersBoard[3][1].getContent()} onHit = {() => onHit(3,1)}/>
+            <Cells content = {computersBoard[3][2].getContent()} onHit = {() => onHit(3,2)}/>
+            <Cells content = {computersBoard[3][3].getContent()} onHit = {() => onHit(3,3)}/>
 
 
             </div>
